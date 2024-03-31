4dffa0545667e3fb541e767551759d51ed61765f
Solorider78 <amirajvad8@gmail.com>
2024-04-01 00:17:00 +0330

    Create Coloured_text.py

---
+++ Coloured_text.py
@@ -0,0 +1,28 @@
+from colorama import Fore
+
+def red_text(text):
+  return f'{Fore.RED}{text}{Fore.RESET}'
+def green_text(text):
+  return f'{Fore.GREEN}{text}{Fore.RESET}'
+
+def black_text(text):
+  return f'{Fore.BLACK}{text}{Fore.RESET}'
+def cyan_text(text):
+  return f'{Fore.CYAN}{text}{Fore.RESET}'
+
+def blue_text(text):
+  return f'{Fore.BLUE}{text}{Fore.RESET}'
+
+def yellow_text(text):
+  return f'{Fore.YELLOW}{text}{Fore.RESET}'
+
+def purple_text(text):
+  return f'{Fore.MAGENTA}{text}{Fore.RESET}'
+
+def main():
+  pass
+
+if __name__ == "__main__":
+  main()
+
+
